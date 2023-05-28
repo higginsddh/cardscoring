5f1e56c911a497aa024a0ffbe39642fed72531d2
Daniel Higgins <daniel.higgins@gmail.com>
2023-05-28 10:03:54 -0400

    liveblocks

---
+++ pages/liveblocks.config.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCK_KEY,
+});
+
+export const { RoomProvider } = createRoomContext(client);
